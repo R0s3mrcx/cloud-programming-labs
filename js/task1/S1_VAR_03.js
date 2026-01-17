0c63e24f1398c9fc89821a02efec2b319037aef6
R0s3mrcx <r0s3mrcx@gmail.com>
2026-01-17 13:19:10 +0100

    TASK1: S1_VAR_03 - const mutation vs reassignment

---
+++ js/task1/S1_VAR_03.js
@@ -0,0 +1,12 @@
+const user = { name: "Ala", tags: [] };
+
+user.tags.push("js");
+user.tags.push("node");
+
+console.log("After mutation:", user);
+
+try {
+  user = {};
+} catch (err) {
+  console.log("Reassignment error:", err.message);
+}
