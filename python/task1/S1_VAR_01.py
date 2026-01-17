baf79bd311b3c7d44ac53f3a2d14d6f73ef801e0
R0s3mrcx <r0s3mrcx@gmail.com>
2026-01-17 17:11:42 +0100

    S1_VAR_01: declare variables and observe types

---
+++ python/task1/S1_VAR_01.py
@@ -0,0 +1,8 @@
+a = "hello"      # str
+b = 42           # int
+c = True         # bool
+
+print("name | value | type")
+print("a |", a, "|", type(a))
+print("b |", b, "|", type(b))
+print("c |", c, "|", type(c))
