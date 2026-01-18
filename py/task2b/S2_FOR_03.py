14037305004eb3e78bcc17f0e26bc051b33ffc7a
R0s3mrcx <r0s3mrcx@gmail.com>
2026-01-18 20:30:42 +0100

    TASK2B: S2_FOR_03 - sum until threshold

---
+++ py/task2b/S2_FOR_03.py
@@ -0,0 +1,11 @@
+def sum_until(nums, threshold):
+    total = 0
+    for n in nums:
+        if total + n > threshold:
+            break
+        total += n
+    return total
+
+
+print(sum_until([1, 2, 3, 4], 5))
+print(sum_until([5, 5, 5], 9))
